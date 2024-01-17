5b5716124440e01a136bc3ad9f1f0f83e953401d
unanottecosi <30680914+unanottecosi@users.noreply.github.com>
2024-01-16 22:53:23 -0800

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+
+
+@pytest.fixture()
+def setup(request):
+    
+#     chrome_options = webdriver.ChromeOptions()
+#     options = [
+#     "--headless=new",
+#     "--disable-gpu",
+#     "--window-size=1920,1200",
+#     "--ignore-certificate-errors",
+#     "--disable-extensions",
+#     "--no-sandbox",
+#     "--disable-dev-shm-usage"
+# ]
+    options = Options()
+    options.add_argument('--headless')
+
+    # for option in options:
+    #     chrome_options.add_argument(option)
+
+    request.cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
+    yield request.cls.driver
+    request.cls.driver.close()
